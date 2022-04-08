5693786b31f06ae74b2a4f9d4cd074dcf912b28e
Aleksandr Kretov <kretov.1988@gmail.com>
2022-04-08 12:53:30 +0500

    Initial Commit

---
+++ NetworkingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingApp
+//
+//  Created by Aleksandr Kretov on 08.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
