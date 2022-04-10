dd0fe77b0da9c677a8990ae3ba134094c88645bb
Aleksandr Kretov <kretov.1988@gmail.com>
2022-04-10 18:14:49 +0500

    Fixes

--- NetworkingApp/Services/NetworkManager.swift
+++ NetworkingApp/Services/NetworkManager.swift
@@ -5,33 +5,36 @@
 //  Created by Aleksandr Kretov on 08.04.2022.
 //
 import Foundation
+import UIKit
 
 class NetworkManager {
-    
     enum Links: String {
-        case randomImage = "https://picsum.phots/1300/2800"
-        case randomImagesList = "https://picsum.phots/v2/list"
+        case randomImage = "https://picsum.photos/1300/2800"
+        case randomImagesList = "https://picsum.photos/v2/list"
     }
 
     static let shared = NetworkManager()
-    
+
     func fetchImage(from url: String, completion: @escaping (Result<Data, Error>) -> Void) {
         guard let url = URL(string: url) else { return }
         URLSession.shared.dataTask(with: url) { data, _, error in
-            if data != nil {
-                completion(.success(data!))
-            } else if let error = error {
-                completion(.failure(error))
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                }
+                return
             }
+            completion(.success(data))
         }.resume()
     }
-    
+
     func fetchImages(from url: String, completion: @escaping (Result<[Image], Error>) -> Void) {
         guard let url = URL(string: url) else { return }
         URLSession.shared.dataTask(with: url) { data, _, error in
             guard
                 let data = data,
-                let images = try? JSONDecoder().decode([Image].self, from: data) else {
+                let images = try? JSONDecoder().decode([Image].self, from: data)
+            else {
                 if let error = error {
                     completion(.failure(error))
                 }
@@ -43,4 +46,3 @@ class NetworkManager {
 
     init() {}
 }
-    
